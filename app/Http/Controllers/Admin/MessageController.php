3628c7af2b2e227fa1da3638a8f8fc83b0441982
andmalt <andrea.maltempi@gmail.com>
2022-03-09 15:15:49 +0100

    inserted admin index messages

--- app/Http/Controllers/Admin/MessageController.php
+++ app/Http/Controllers/Admin/MessageController.php
@@ -6,12 +6,18 @@ use App\Http\Controllers\Controller;
 use App\Models\Apartment;
 use App\Models\Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MessageController extends Controller
 {
     public function getMessages(Apartment $apartment)
     {
-        return view('admin.messages.index',compact('apartment'));
+        
+        if($apartment->user_id == Auth::user()->id){
+            return view('admin.messages.index', compact('apartment'));
+        }else{
+            return view('guest.welcome');
+        }       
     }
 
     public function viewMessage(Message $message)
--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -1,28 +1,31 @@
 @extends('admin.dashboard')
 
 @section('content')
-    <div class="mb-5 p-2">
+    <div class="mb-8 p-2">
         <h2 class="mb-3"> <strong>Ecco tutti i messaggi della casa:</strong> </h2>
-        <h1 class="uppercase">{{$apartment->title}}</h1>
+        <h1 class="uppercase font-semibold">{{$apartment->title}}</h1>
     </div>
 
     {{-- session --}}
     {{-- @if (Session::has('delete-image'))
         <p class="text-red-700 py-2 my-4">{{Session::get('delete-image')}}</p>
     @endif --}}
-
-    <div class="my-5 p-2 flex flex-row flex-wrap">
-        <div class="w-full">
-
-        </div>
-    </div>
+    
+    @php
+        $i=1;
+    @endphp
     <div class="flex flex-row flex-wrap">
         @forelse ($apartment->messages as $message)
-            <div class="shadow-xl m-2 rounded-md">  
+            <div class="mb-8">
+                <h5><span class="uppercase">Messaggio numero:</span> <span class="font-bold">{{$i++}}</span></h5>
+            </div>
+            <div class="shadow-xl mb-6 rounded-md p-5">  
                 <div class="">
-                    <h1>{{$message->title}}</h1>
+                    <h3> <span class="font-semibold"> Scritto da: </span> <span class="underline">{{$message->name}} {{$message->surname}}</span> </h3>
+                    <h4 class="mb-4"> <span class="font-semibold">Email: </span> <span class="underline">{{$message->email}}</span> </h4>
+                    <p>{{$message->message_content}}</p>
                 </div>             
-                <div class="text-center my-3">
+                <div class="text-center my-5">
                     <form class="delete_image" action="{{route('admin.message.destroy', $message->id )}}" method="post">
                         @csrf
                         @method('DELETE')
@@ -34,7 +37,7 @@
             <h3>Questa casa non ha messaggi</h3>
         @endforelse
     </div>
-    <div class="text-center mt-6 p-2">
+    <div class="mt-6 p-2">
         <a class="bg-yellow-500 hover:bg-yellow-700 hover:text-white rounded-lg py-2 px-4" href="{{route('admin.apartment.show',$apartment->id)}}">torna nella pagina della casa</a>
     </div>
 @endsection
