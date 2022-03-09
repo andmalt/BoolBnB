31c2e64cc8e3697a7f286cd24caf9955dff5e7a7
andmalt <andrea.maltempi@gmail.com>
2022-03-09 16:57:09 +0100

    fixed index messages and inserted show message

--- app/Http/Controllers/Admin/MessageController.php
+++ app/Http/Controllers/Admin/MessageController.php
@@ -22,11 +22,18 @@ class MessageController extends Controller
 
     public function viewMessage(Message $message)
     {
-        //
+
+        if ($message->apartment->user_id == Auth::user()->id) {
+            return view('admin.messages.show', compact('message'));
+        } else {
+            return view('guest.welcome');
+        }  
     }
 
     public function deleteMessage(Message $message)
     {
         $message->delete();
+
+        return \redirect()->route('admin.messages.index', $message->apartment->id)->with('delete-message',"Il messaggio di $message->name $message->surname è stato cancellato con successo");
     }
 }
--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -7,36 +7,48 @@
     </div>
 
     {{-- session --}}
-    {{-- @if (Session::has('delete-image'))
-        <p class="text-red-700 py-2 my-4">{{Session::get('delete-image')}}</p>
-    @endif --}}
-    
+    @if (Session::has('delete-message'))
+        <p class="text-red-700 py-2 my-4">{{Session::get('delete-message')}}</p>
+    @endif
+
     @php
         $i=1;
     @endphp
-    <div class="flex flex-row flex-wrap">
-        @forelse ($apartment->messages as $message)
-            <div class="mb-8">
-                <h5><span class="uppercase">Messaggio numero:</span> <span class="font-bold">{{$i++}}</span></h5>
-            </div>
-            <div class="shadow-xl mb-6 rounded-md p-5">  
-                <div class="">
-                    <h3> <span class="font-semibold"> Scritto da: </span> <span class="underline">{{$message->name}} {{$message->surname}}</span> </h3>
-                    <h4 class="mb-4"> <span class="font-semibold">Email: </span> <span class="underline">{{$message->email}}</span> </h4>
-                    <p>{{$message->message_content}}</p>
-                </div>             
-                <div class="text-center my-5">
+    <table class="shadow-2xl border border-slate-300 w-full table-auto">
+        <thead class="mb-4">
+            <tr class="border-slate-300 border bg-slate-200">
+                <th class="p-4">#</th>
+                <th class="p-4">Nome e Cognome</th>
+                <th class="p-4">Email</th>
+                <th class="p-4">Creato in data</th>
+                <th class="px-12"></th>
+            </tr>
+        </thead>
+        <tbody class="">
+            @forelse ($apartment->messages as $message)
+            <tr class="border-slate-300 border">
+                <td class="font-bold p-4">{{$i++}}</td>
+                <td class="p-4">{{$message->name}} {{$message->surname}}</td>
+                <td class="p-4">{{$message->email}}</td>
+                <td class="p-4">{{$message->created_at}}</td>
+                <td class="flex justify-center items-center flex-wrap p-4">
+                    <a class="bg-blue-500 py-2 px-4 rounded-md mr-3 my-3" href="{{route('admin.message.show',$message->id)}}">visualizza</a>
                     <form class="delete_image" action="{{route('admin.message.destroy', $message->id )}}" method="post">
                         @csrf
                         @method('DELETE')
-                        <button class="bg-red-700 hover:bg-red-800 rounded-md py-2 px-4 hover:text-white " type="submit">cancella messaggio</button>
+                        <button class="bg-red-700 hover:bg-red-800 rounded-md py-2 px-4 hover:text-white" type="submit">cancella messaggio</button>
                     </form> 
-                </div>   
-            </div>
-        @empty
-            <h3>Questa casa non ha messaggi</h3>
-        @endforelse
-    </div>
+                </td>
+            </tr>
+            @empty
+            <tr>
+                <td class="p-4 font-bold">Non ci sono Messaggi per questa casa</td>
+            </tr>    
+            @endforelse          
+        </tbody>
+    </table>
+
+
     <div class="mt-6 p-2">
         <a class="bg-yellow-500 hover:bg-yellow-700 hover:text-white rounded-lg py-2 px-4" href="{{route('admin.apartment.show',$apartment->id)}}">torna nella pagina della casa</a>
     </div>
