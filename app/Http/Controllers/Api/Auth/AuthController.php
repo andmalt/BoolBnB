7d112bde9d64d1dfc543ae440222c3dbc0c8ed70
andmalt <andrea.maltempi@gmail.com>
2022-12-26 16:55:54 +0100

    Fixed register function in authController and added menu in header navigation

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -12,24 +12,13 @@ class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        $validator = Validator::make($request->all(), [
+
+        $fields = $request->validate([
             'name' => 'required|string|max:255|min:2',
             'surname' => 'required|string|max:255|min:2',
             'email' => 'required|string|email|max:255|unique:users,email',
             'password' => 'required|string|confirmed|max:30'
         ]);
-
-        if ($validator->fails()) {
-
-            $response = [
-                'success' => false,
-                'errors' => $validator->errors(),
-            ];
-
-            return response()->json($response, 400);
-        }
-
-        $fields = $validator;
 
         $user = User::create([
             'name' => $fields['name'],
--- resources/js/app/layout/Header.tsx
+++ resources/js/app/layout/Header.tsx
@@ -4,6 +4,9 @@ import { Link, useNavigate } from 'react-router-dom';
 import api from '../services/connection_manager';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
 import { clear, logout, loading, error } from '../store/authSlice';
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
 
 interface HeaderProps {
 }
@@ -31,6 +34,10 @@ const Header = (props: HeaderProps) => {
     }
   }
 
+  function classNames(...classes:any) {
+    return classes.filter(Boolean).join(' ')
+  }
+
   useEffect(() => {
     if (isMount) {
       // 
@@ -50,16 +57,70 @@ const Header = (props: HeaderProps) => {
               {
                 authSelector.token != null ?
                   <>
-                    <Link to="/dashboard" className='text-blue-800 hover:text-blue-600 
-                      transition ease-in-out delay-150 hover:-translate-y-1 font-bold'>
-                      Dashboard
-                    </Link>
-                    <button
-                      className='text-blue-800 hover:text-blue-600 
-                      transition ease-in-out delay-150 hover:-translate-y-1 font-bold'
-                      onClick={(e) => setLogout(e)}>
-                      Esci
-                    </button>
+                    <Menu as="div" className="relative inline-block text-left">
+                      <div>
+                        <Menu.Button className="font-bold inline-flex w-full justify-center rounded-md border border-[rgba(0,0,0,0)] bg-black px-4 py-2 text-sm text-blue-800 shadow-sm hover:border-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-800 focus:ring-offset-2 focus:ring-offset-blue-800">
+                          Menu
+                          <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+                        </Menu.Button>
+                      </div>
+
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-black shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          <div className="py-1">
+                            <Menu.Item>
+                              {({ active }) => (
+                                <Link
+                                  to={"/dashboard"}
+                                  className={classNames(
+                                    active ? 'bg-blue-800 text-white' : 'text-blue-800',
+                                    'block px-4 py-2 text-sm'
+                                  )}
+                                >
+                                  Dashboard
+                                </Link>
+                              )}
+                            </Menu.Item>
+                            <Menu.Item>
+                              {({ active }) => (
+                                <a
+                                  href="#"
+                                  className={classNames(
+                                    active ? 'bg-blue-800 text-white' : 'text-blue-800',
+                                    'block px-4 py-2 text-sm'
+                                  )}
+                                >
+                                  Support
+                                </a>
+                              )}
+                            </Menu.Item>
+                            <form method="POST" onSubmit={(e) => setLogout(e)}>
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <button
+                                    type="submit"
+                                    className={classNames(
+                                      active ? 'bg-blue-800 text-white' : 'text-blue-800',
+                                      'block w-full px-4 py-2 text-left text-sm'
+                                    )}
+                                  >
+                                    Esci
+                                  </button>
+                                )}
+                              </Menu.Item>
+                            </form>
+                          </div>
+                        </Menu.Items>
+                      </Transition>
+                    </Menu>
                   </>
                   :
                   <>
